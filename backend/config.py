dc78304c8c004b5401eca0a0315dc4f92f8d61b5
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-08-28 01:19:58 -0400

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATABASE_URL = "sqlite:///users.db"
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "jwtsecretkey"
