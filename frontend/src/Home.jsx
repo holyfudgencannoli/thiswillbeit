268b99867b16fa5c076553004065b712b66c6f63
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-08-28 01:25:40 -0400

    Create Home.jsx

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
