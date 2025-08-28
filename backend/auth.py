b261804da2c718328e2904d05640afe650117adb
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-08-28 01:21:05 -0400

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,26 +1,23 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from models import User, db
+from models import create_user, get_user_by_username, check_password
 
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    if User.query.filter_by(username=data['username']).first():
+    if get_user_by_username(data['username']):
         return jsonify({"msg": "User already exists"}), 400
-    user = User(username=data['username'])
-    user.set_password(data['password'])
-    db.session.add(user)
-    db.session.commit()
+    create_user(data['username'], data['password'])
     return jsonify({"msg": "User created"}), 201
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    user = User.query.filter_by(username=data['username']).first()
-    if user and user.check_password(data['password']):
-        access_token = create_access_token(identity=user.id)
+    user = get_user_by_username(data['username'])
+    if user and check_password(user, data['password']):
+        access_token = create_access_token(identity=user['id'])
         return jsonify(access_token=access_token)
     return jsonify({"msg": "Bad username or password"}), 401
 
